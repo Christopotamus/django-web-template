57458189ca18fd1c201d0738296ed141eb84213d
Christopher Budd <chris.budd@me.com>
2012-08-20 15:46:07 -0500

    Modified templates and settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -68,7 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__),'static')
+    os.path.join(os.path.dirname(__file__),'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -108,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__),'templates')
+    os.path.join(os.path.dirname(__file__),'templates'),
 )
 
 INSTALLED_APPS = (
